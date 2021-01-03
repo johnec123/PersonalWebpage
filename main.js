a19511767d18d680bc5a2920082db05913d56bb8
John <johnecorry@gmail.com>
2021-01-03 19:08:48 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+/**
+ * Template Name: MyResume - v3.0.0
+ * Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
+!(function($) {
+    $(document).on('click', '.mobile-nav-toggle', function(e) {
+        $('body').toggleClass('mobile-nav-active');
+        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+    });
+
+    $(document).click(function(e) {
+        var container = $(".mobile-nav-toggle");
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            if ($('body').hasClass('mobile-nav-active')) {
+                $('body').removeClass('mobile-nav-active');
+                $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+            }
+        }
+    });
+})(jQuery);
